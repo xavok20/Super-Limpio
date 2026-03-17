7e007701cb2ad5eba2f6dcae33ee62e745fd60c5
Alexander Chantera <xhavok007@gmail.com>
2026-03-16 22:45:04 -0500

    Update logo source to use Vite static asset handling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,5 @@
+import logoUrl from '/logo-super-limpio.png?url' // El ?url asegura que Vite lo trate como una ruta estática
+
 interface LogoProps {
   className?: string;
   size?: number;
@@ -6,7 +8,7 @@ interface LogoProps {
 export function Logo({ className = "", size = 40 }: LogoProps) {
   return (
     <img 
-      src="/logo-super-limpio.png" 
+      src={logoUrl} 
       alt="Super Limpio Logo" 
       width={size}
       height={size}
