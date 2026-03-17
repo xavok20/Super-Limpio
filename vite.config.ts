e97cc41997ffe80a5f242f006abc9fa7506557c5
Alexander Chantera <xhavok007@gmail.com>
2026-03-16 22:20:05 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base:'/super-limpio/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
